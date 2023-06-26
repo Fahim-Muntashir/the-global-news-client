002f5602845448bfab6260acd0c20bda08eb766a
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-06-26 12:50:25 +0600

    news added

--- src/Pages/Home/NewsCard/NewsCard.js
+++ src/Pages/Home/NewsCard/NewsCard.js
@@ -1,8 +1,28 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
 
 const NewsCard = ({ news }) => {
-  const { _id, title, details } = { news };
-  return <div></div>;
+  const { _id, title, details, image_url } = news;
+  return (
+    <div>
+      <Card className="mb-4">
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Img variant="top" src={image_url} />
+          <Card.Text>
+            {details.length < 250 ? (
+              <>{details}</>
+            ) : (
+              <>{details.slice(0, 250)}</>
+            )}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
+    </div>
+  );
 };
 
 export default NewsCard;
