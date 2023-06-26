e1b0190f2fcb44028bf2bdb460304ec3a1e09c19
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-06-26 15:49:01 +0600

    details page and suggest section added

--- src/Pages/Home/NewsCard/NewsCard.js
+++ src/Pages/Home/NewsCard/NewsCard.js
@@ -1,12 +1,36 @@
+import moment from "moment";
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Image } from "react-bootstrap";
+import {
+  FaEye,
+  FaRegBookmark,
+  FaRegStar,
+  FaShareAlt,
+  FaStar,
+} from "react-icons/fa";
+import Rating from "react-rating";
+import { Link } from "react-router-dom";
 
 const NewsCard = ({ news }) => {
-  const { _id, title, details, image_url } = news;
+  const { _id, title, details, image_url, rating, author, total_view } = news;
   return (
     <div>
       <Card className="mb-4">
-        <Card.Header>Featured</Card.Header>
+        <Card.Header className="d-flex align-items-center">
+          <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
+          <div className="ps-2 flex-grow-1">
+            <p className="mb-0">{author?.name}</p>
+            <p className="mt-0">
+              <small>
+                {moment(author?.published_date).format("yyyy-MMM-D")}
+              </small>
+            </p>
+          </div>
+          <div>
+            <FaRegBookmark></FaRegBookmark>
+            <FaShareAlt></FaShareAlt>
+          </div>
+        </Card.Header>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Img variant="top" src={image_url} />
@@ -14,12 +38,30 @@ const NewsCard = ({ news }) => {
             {details.length < 250 ? (
               <>{details}</>
             ) : (
-              <>{details.slice(0, 250)}</>
+              <>
+                {details.slice(0, 250)}...{" "}
+                <Link to={`/news/${_id}`}>Read More</Link>{" "}
+              </>
             )}
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
-        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        <Card.Footer className="text-muted d-flex">
+          <div className="flex-grow-1">
+            <span>{rating?.number}</span>
+
+            <Rating
+              placeholderRating={rating.number}
+              emptySymbol={<FaRegStar></FaRegStar>}
+              placeholderSymbol={<FaStar className="text-waring"></FaStar>}
+              readonly
+              fullSymbol={<FaStar></FaStar>}
+            />
+          </div>
+          <div>
+            <FaEye></FaEye> {total_view}
+          </div>
+        </Card.Footer>
       </Card>
     </div>
   );
